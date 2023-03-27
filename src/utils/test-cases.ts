8fd568f2f95e1e3a53fc807bb680be697f925b3a
zxisatree <62459441+zxisatree@users.noreply.github.com>
2023-03-27 17:14:09 +0800

    Rename testCases to test-cases

---
+++ src/utils/test-cases.ts
@@ -0,0 +1,23 @@
+import { arrayTests } from '../__tests__/arrays'
+import { basicTests } from '../__tests__/basic'
+import { conditionalTests } from '../__tests__/conditionals'
+import { functionTests } from '../__tests__/functions'
+import { generalTests } from '../__tests__/general'
+import { loopTests } from '../__tests__/loops'
+import { operatorTests } from '../__tests__/operators'
+import { pointerTests } from '../__tests__/pointers'
+import { primitiveFunctionsTests } from '../__tests__/primitiveFunctions'
+import { typeTests } from '../__tests__/types'
+
+export default {
+  ...arrayTests,
+  ...basicTests,
+  ...conditionalTests,
+  ...functionTests,
+  ...generalTests,
+  ...loopTests,
+  ...operatorTests,
+  ...pointerTests,
+  ...primitiveFunctionsTests,
+  ...typeTests
+}
